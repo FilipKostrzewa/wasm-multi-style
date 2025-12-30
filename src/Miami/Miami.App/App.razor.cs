043e40e7a48caabc0d048d1a76df5051b563be5c
Filip Kostrzewa <>
2025-12-30 07:37:17 +0100

    link 3 micro-fe with the app

---
+++ src/Miami/Miami.App/App.razor.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace Miami.App;
+
+public partial class App
+{
+    private readonly Assembly _appAssembly = typeof(App).Assembly;
+
+    private readonly Assembly[] _additionalAssemblies = new Assembly[]
+    {
+        typeof(Miami.Pages.One.PageOne).Assembly,
+        typeof(Miami.Pages.Two.PageTwo).Assembly,
+        typeof(Miami.Pages.Three.PageThree).Assembly,
+    };
+
+}
